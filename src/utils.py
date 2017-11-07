d58d03f0de616ae359bbaaeedaca7ef5ca94d1dc
tobijjah <tobias.seydewitz@posteo.net>
2017-11-07 20:46:23 +0100

    Added method for fetching dir tree

--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,19 @@ Mail: tobi.seyde@gmail.com
 
 Description:
 """
+import os
+from pathlib import Path
+from collections import namedtuple
+
+
+def get_data_dir(path):
+    dir_structure = {
+        os.path.split(root)[-1]: Path(root)
+        for root, *_ in os.walk(path)
+    }
+    Directories = namedtuple('Directories', dir_structure.keys())
+    return Directories(**dir_structure)
+
+
+if __name__ == '__main__':
+    pass
