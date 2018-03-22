923bb1cb00101368528918679a6a0516ec050a58
tobijjah <tobias.seydewitz@posteo.net>
2018-03-22 13:02:58 +0100

    Initial commit

---
+++ src/cell.py
@@ -0,0 +1,24 @@
+"""
+cell.py
+
+Author: Tobias Seydewitz
+Date: 22.03.18
+Mail: tobi.seyde@gmail.com
+"""
+
+
+class Cell:
+    __slots__ = 'x y value'
+
+    def __init__(self, x, y, value):
+        self.x = x
+        self.y = y
+        self.value = value
+
+    @property
+    def row(self):
+        return self.y
+
+    @property
+    def col(self):
+        return self.x
