5583595035ff9420effb9c203f023008230d21b4
tobijjah <tobias.seydewitz@posteo.net>
2018-04-05 14:16:32 +0200

    Doc

--- tests/utilities.py
+++ tests/utilities.py
@@ -8,7 +8,20 @@ Mail: tobi.seyde@gmail.com
 import numpy as np
 
 
-def test_data(size=(100, 100)):
+def random_test_data(size=(100, 100)):
+    """
+    Creates random test data sets, data properties meet Hansen et al. and
+    Chen et al..
+
+    :param size: numeric tuple
+        Shape of test data, default is a 100x100 2D array.
+    :return: treecover, loss, gain, gl30_00, gl30_10
+        The returned data has the dtype uint8 and dimensionality of size.
+        Treecover, value range 0 to 100. Loss, value range 0-12, values between 1 to 12
+        appear only within treecover 1 to 100. Gain, value range 0-1 randomly distributed.
+        GL30_00, value range 0,10,20,30,40,50,60,70,80,90,100, value 20 appears only within
+        treecover > 30.
+    """
     # tree distribution
     distribution = np.random.uniform(size=size)
 
