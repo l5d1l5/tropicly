3abae88c4f1edb8128dcf30d10bf10f00ce737d1
tobijjah <tobias.seydewitz@posteo.net>
2018-04-06 22:46:29 +0200

    seed

--- tests/utilities.py
+++ tests/utilities.py
@@ -8,7 +8,7 @@ Mail: tobi.seyde@gmail.com
 import numpy as np
 
 
-def random_test_data(size=(100, 100)):
+def random_test_data(size=(100, 100), seed=42):
     """
     Creates random test data sets, data properties meet Hansen et al. and
     Chen et al..
@@ -22,29 +22,31 @@ def random_test_data(size=(100, 100)):
         GL30_00, value range 0,10,20,30,40,50,60,70,80,90,100, value 20 appears only within
         treecover > 30.
     """
+    random = np.random.RandomState(seed)
+
     # tree distribution
-    distribution = np.random.uniform(size=size)
+    distribution = random.uniform(size=size)
 
     # Hansen treecover
-    treecover = np.random.uniform(size=size) * 100
+    treecover = random.uniform(size=size) * 100
     treecover = np.round_(treecover)
     treecover[distribution > .5] = 0
 
     # Hansen loss
-    loss = np.random.choice([i for i in range(1, 13)], size=size)
+    loss = random.choice([i for i in range(1, 13)], size=size)
     loss[treecover == 0] = 0
     loss[distribution > .3] = 0
 
     # Gain
-    gain = np.random.randint(2, size=size)
+    gain = random.randint(2, size=size)
     gain[distribution > .6] = 0
 
     # GL30 2000
-    gl30_00 = np.random.choice([i * 10 for i in range(11) if i != 2], size=size)
+    gl30_00 = random.choice([i * 10 for i in range(11) if i != 2], size=size)
     gl30_00[treecover > 30] = 20
 
     # GL30 2010
-    gl30_10 = np.random.choice([i * 10 for i in range(11)], size=size)
+    gl30_10 = random.choice([i * 10 for i in range(11)], size=size)
 
     return (treecover.astype(np.uint8),
             loss.astype(np.uint8),
