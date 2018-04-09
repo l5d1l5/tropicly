9d4c56037c4ca1f4ad0284399c2665b4b554ac8a
tobijjah <tobias.seydewitz@posteo.net>
2018-04-09 11:28:11 +0200

    Initial commit

---
+++ tropicly/classification.py
@@ -0,0 +1,7 @@
+"""
+classification.py
+
+Author: Tobias Seydewitz
+Date: 09.04.18
+Mail: tobi.seyde@gmail.com
+"""
---
+++ tropicly/sampling.py
@@ -0,0 +1,7 @@
+"""
+sampling.py
+
+Author: Tobias Seydewitz
+Date: 09.04.18
+Mail: tobi.seyde@gmail.com
+"""
