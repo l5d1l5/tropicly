5ff1fc2d8fb4c7bc40780c79f5a1fce45b4dd3e3
tobijjah <tobias.seydewitz@posteo.net>
2018-04-10 10:45:29 +0200

    Add sampling worker

--- tropicly/__init__.py
+++ tropicly/__init__.py
@@ -5,9 +5,11 @@ Author: Tobias Seydewitz
 Date: 06.04.18
 Mail: tobi.seyde@gmail.com
 """
+from .sampling import worker as sworker
 from .harmonization import worker as hworker
 
 
 __all__ = [
     'hworker',
+    'sworker'
 ]
