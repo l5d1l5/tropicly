297d3de503c3e1b13e2f1b05579ba711ed2cfd04
tobijjah <tobias.seydewitz@posteo.net>
2018-04-11 10:36:54 +0200

    Add freq worker

--- tropicly/__init__.py
+++ tropicly/__init__.py
@@ -6,12 +6,14 @@ Date: 06.04.18
 Mail: tobi.seyde@gmail.com
 """
 from .sampling import worker as sworker
+from .frequency import worker as fworker
 from .harmonization import worker as hworker
 
 
 __version__ = 0.1
 
 __all__ = [
+    'sworker',
+    'fworker',
     'hworker',
-    'sworker'
 ]
