7fe3d12944f00ac32e003f1c3640ae8edef03b13
tobijjah <tobias.seydewitz@posteo.net>
2018-04-12 13:51:48 +0200

    Add distance

--- tropicly/__init__.py
+++ tropicly/__init__.py
@@ -5,6 +5,7 @@ Author: Tobias Seydewitz
 Date: 06.04.18
 Mail: tobi.seyde@gmail.com
 """
+from .distance import Distance
 from .sampling import worker as sworker
 from .frequency import worker as fworker
 from .harmonization import worker as hworker
@@ -12,7 +13,9 @@ from .harmonization import worker as hworker
 
 __version__ = 0.1
 
+
 __all__ = [
+    'Distance',
     'sworker',
     'fworker',
     'hworker',
