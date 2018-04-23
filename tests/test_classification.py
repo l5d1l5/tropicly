85c6c475ad25e25aca6c07c0f0150d983eb9c625
tobijjah <tobias.seydewitz@posteo.net>
2018-04-23 13:56:35 +0200

    Some tests

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -6,9 +6,13 @@ Date: 10.04.18
 Mail: tobi.seyde@gmail.com
 """
 import numpy as np
+from affine import Affine
 from unittest import TestCase
+from tests.utilities import random_test_data
 from tropicly.classification import (extract_square,
-                                     circle_mask)
+                                     superimpose,
+                                     circle_mask,
+                                     reclassify)
 
 
 class TestClassification(TestCase):
@@ -28,3 +32,61 @@ class TestClassification(TestCase):
 
         self.assertEqual(expected, actual.shape)
 
+    def test_superimpose_with_malicious_shapes(self):
+        a = np.zeros((10, 10), dtype=np.uint8)
+        treecover, loss, gain, *_ = random_test_data()
+
+        with self.assertRaises(ValueError) as err:
+            superimpose(a, treecover, gain, loss)
+
+    def test_superimpose_with_equal_shaped(self):
+        treecover, loss, gain, _, gl30_10 = random_test_data()
+
+        expected = (100, 100)
+        actual = superimpose(gl30_10, treecover, gain, loss)
+
+        self.assertEqual(expected, actual.shape)
+        self.assertEqual(np.uint8, actual.dtype)
+
+    def test_superimpose_with_valid_data(self):
+        treecover, loss, gain, _, gl30_10 = random_test_data((15, 15))
+
+        expected = np.array([[0, 0, 0, 0, 25, 20, 25, 0, 0, 0, 0, 0, 0, 0, 25],
+                             [25, 0, 0, 0, 0, 0, 0, 25, 0, 0, 0, 25, 0, 0, 25],
+                             [0, 90, 25, 0, 0, 0, 0, 40, 0, 0, 80, 0, 25, 0, 30],
+                             [0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 70, 0],
+                             [0, 60, 0, 0, 50, 0, 30, 0, 0, 0, 0, 0, 25, 0, 0],
+                             [0, 0, 30, 0, 50, 0, 0, 0, 80, 0, 0, 0, 0, 0, 0],
+                             [40, 0, 0, 0, 0, 0, 0, 0, 40, 60, 25, 0, 0, 0, 0],
+                             [25, 0, 0, 0, 20, 25, 30, 0, 0, 0, 0, 0, 25, 0, 0],
+                             [0, 0, 0, 20, 25, 0, 0, 0, 50, 0, 0, 25, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 100, 0, 0, 80, 90, 0, 0, 80, 25],
+                             [0, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 25],
+                             [0, 0, 40, 25, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 25],
+                             [0, 25, 0, 0, 25, 0, 0, 0, 0, 100, 90, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 60, 40, 0, 0, 0, 25, 0, 100, 60],
+                             [0, 0, 0, 20, 0, 25, 0, 0, 0, 0, 0, 0, 20, 0, 100]], dtype=np.uint8)
+        actual = superimpose(gl30_10, treecover, gain, loss)
+
+        self.assertTrue(np.array_equal(expected, actual))
+
+    def test_reclassify(self):
+        affine = Affine(0.00027041676761241124, 0.0, 120.0000008, 0.0, -0.00027041676761241124, 4.9999992)
+
+        driver = np.array([[0, 0, 0, 0,30,20,20, 0, 0, 0, 0, 0, 0, 0,25],
+                           [0, 0, 0, 0,30,20,30, 0, 0, 0, 0,25, 0, 0,25],
+                           [0, 0, 0, 0,30,20,30, 0, 0, 0,80, 0,25, 0,30],
+                           [0, 0, 0, 0,25, 0, 0, 0, 0, 0, 0, 0, 0,70, 0],
+                           [0,60, 0, 0,50, 0,30, 0, 0, 0, 0, 0,25, 0, 0],
+                           [0, 0,30, 0,50, 0, 0, 0,80, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0,40,60,25, 0, 0, 0, 0],
+                           [0, 0, 0, 0,20,25,30, 0, 0, 0, 0, 0,25, 0, 0],
+                           [0, 0, 0,20,25, 0, 0, 0,50, 0, 0,25, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0,10, 0, 0,80,90, 0, 0,80,25],
+                           [0, 0, 0, 0, 0,25, 0, 0, 0, 0, 0, 0, 0, 0,25],
+                           [0, 0,40,25, 0, 0, 0, 0,25, 0, 0, 0, 0, 0,25],
+                           [0,25, 0, 0,25, 0, 0, 0, 0,10,90, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0,60,40, 0, 0, 0,25,20,10,60],
+                           [0, 0, 0,20, 0,25, 0, 0, 0, 0, 0, 0,20, 0,10]], dtype=np.uint8)
+
+        print(reclassify(driver, buffer_size=500, affine=affine))
