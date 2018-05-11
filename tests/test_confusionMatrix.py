9a466587a45d24bcdeb09d947ea0e4fd571bada9
tobijjah <tobias.seydewitz@posteo.net>
2018-05-11 09:37:47 +0200

    Tests

--- tests/test_confusionMatrix.py
+++ tests/test_confusionMatrix.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 import numpy as np
-from tropicly.confusion_matrix import ConfusionMatrix
+from tropicly.confusion_matrix import ConfusionMatrix, ConfusionMatrixLabelError
 
 
 class TestConfusionMatrix(TestCase):
@@ -21,12 +21,12 @@ class TestConfusionMatrix(TestCase):
     def test_confusionMatrix_from_records(self):
         cm = ConfusionMatrix.from_records(self.records)
 
-        self.assertTrue(np.array_equal(self.expected, cm.matrix))
+        self.assertTrue(np.array_equal(self.expected, cm._matrix))
 
     def test_confusionMatrix_from_lists(self):
         cm = ConfusionMatrix.from_lists(self.reference, self.prediction)
 
-        self.assertTrue(np.array_equal(self.expected, cm.matrix))
+        self.assertTrue(np.array_equal(self.expected, cm._matrix))
 
     def test_confusionMatrix_with_char_labels(self):
         ref = np.array(self.reference, dtype=np.unicode)
@@ -41,13 +41,25 @@ class TestConfusionMatrix(TestCase):
 
         cm = ConfusionMatrix.from_lists(ref, pre)
 
-        self.assertTrue(np.array_equal(self.expected, cm.matrix))
+        self.assertTrue(np.array_equal(self.expected, cm._matrix))
 
-    def test_add(self):
-        self.fail()
+    def test_malicious_labels(self):
+        with self.assertRaises(ConfusionMatrixLabelError) as err:
+            ConfusionMatrix([1, 1])
+
+    def test_add_with_digi_cm(self):
+        for val in self.records:
+            self.digi_cm.add(*val)
+
+        self.assertTrue(np.array_equal(self.expected, self.digi_cm._matrix))
+
+    def test_add_with_malicious_values(self):
+        with self.assertRaises(ConfusionMatrixLabelError) as err:
+            for val in self.records:
+                self.char_cm.add(*val)
 
     def test_normalize(self):
-        self.fail()
+        pass
 
-    def test_as_dataframe(self):
-        self.fail()
+    def test_as_df(self):
+        pass
