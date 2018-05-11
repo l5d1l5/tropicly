56a1493945ee8b77bb107c64e1613f20626bfb81
tobijjah <tobias.seydewitz@posteo.net>
2018-05-11 10:41:34 +0200

    Initial commit

---
+++ tests/test_signal.py
@@ -0,0 +1,22 @@
+"""
+test_signal
+
+Author: Tobias Seydewitz
+Date: 11.05.18
+Mail: tobi.seyde@gmail.com
+"""
+from unittest import TestCase
+
+
+class TestSignal(TestCase):
+    def test_connect(self):
+        self.fail()
+
+    def test_remove(self):
+        self.fail()
+
+    def test_fire(self):
+        self.fail()
+
+    def test_clear_all(self):
+        self.fail()
