6fe3f04d40964a1eb834ea9ba17bcdd33c8438cd
tobijjah <tobias.seydewitz@posteo.net>
2018-05-11 13:03:41 +0200

    Doc

--- tropicly/observer.py
+++ tropicly/observer.py
@@ -8,23 +8,66 @@ Mail: tobi.seyde@gmail.com
 
 
 class Signal:
-    def __init__(self, name):
+    """
+    Simple implementation of observer pattern.
+
+    Example:
+        class Observable:
+            def __init__(self):
+                self.on_action = Signal('on_action')
+            def method(self):
+                self.on_action.fire(*args, **kwargs)
+
+        callback(*args, **kwargs):
+            print(*args, **kwargs)
+
+        obj = Observable()
+        obj.on_action.connect(callback)
+    """
+    def __init__(self, name=''):
+        """
+        Signal constructor.
+
+        :param name: str
+            Signal name.
+        """
         self.name = name
         self.__handler = []
 
     def connect(self, handler):
+        """
+        Add a new signal handler.
+
+        :param handler: function or method
+        """
         if handler not in self.__handler:
             self.__handler.append(handler)
 
     def remove(self, handler):
+        """
+        Remove a signal handler.
+
+        :param handler: function or method
+        """
         if handler in self.__handler:
             self.__handler.remove(handler)
 
     def fire(self, *args, **kwargs):
+        """
+        Execute all connected handlers.
+
+        :param args:
+            Handler arguments.
+        :param kwargs:
+            Handler key word arguments.
+        """
         for handler in self.__handler:
             handler(*args, **kwargs)
 
     def clear_all(self):
+        """
+        Disconnect all handlers.
+        """
         self.__handler = []
 
     def __str__(self):
