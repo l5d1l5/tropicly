9878754af5d9d42c891828d988e9c3207c446ff4
tobijjah <tobias.seydewitz@posteo.net>
2018-05-14 14:26:29 +0200

    Initial commit

---
+++ tropicly/abm_emissions.py
@@ -0,0 +1,7 @@
+"""
+abm_emissions.py
+
+Author: Tobias Seydewitz
+Date: 14.05.18
+Mail: tobi.seyde@gmail.com
+"""
---
+++ tropicly/soc_emissions.py
@@ -0,0 +1,7 @@
+"""
+soc_emissions.py
+
+Author: Tobias Seydewitz
+Date: 14.05.18
+Mail: tobi.seyde@gmail.com
+"""
