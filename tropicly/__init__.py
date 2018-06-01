31f8b8e834788a66dcde2fbcee26e70db92625ca
tobijjah <tobias.seydewitz@posteo.net>
2018-06-01 11:55:05 +0200

    Imports

--- tropicly/__init__.py
+++ tropicly/__init__.py
@@ -5,7 +5,6 @@ Author: Tobias Seydewitz
 Date: 06.04.18
 Mail: tobi.seyde@gmail.com
 """
-from tropicly.distance import Distance
 from tropicly.sheduler import TaskSheduler
 from tropicly.confusion_matrix import ConfusionMatrix
 from tropicly.sampling import worker as sworker
@@ -13,13 +12,13 @@ from tropicly.frequency import worker as fworker
 from tropicly.harmonization import worker as hworker
 from tropicly.classification import worker as cworker
 from tropicly.agb_emissions import worker as agbworker
+from tropicly.soc_emissions import worker as socworker
 
 
 __version__ = 0.1
 
 
 __all__ = [
-    'Distance',
     'TaskSheduler',
     'ConfusionMatrix',
     'sworker',
@@ -27,4 +26,5 @@ __all__ = [
     'hworker',
     'cworker',
     'agbworker',
+    'socworker'
 ]
