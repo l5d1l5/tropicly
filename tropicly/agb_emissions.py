82fb4870b080a890ffa5023df4fadf9f107d3218
tobijjah <tobias.seydewitz@posteo.net>
2018-06-04 13:00:59 +0200

    Doc

--- tropicly/agb_emissions.py
+++ tropicly/agb_emissions.py
@@ -18,10 +18,14 @@ LOGGER.addHandler(logging.NullHandler())
 
 def worker(driver, biomass, filename):
     """
+    Worker function for parallel execution.
 
     :param driver: str
+        Path to deforestation driver image.
     :param biomass: str
+        Path to above ground biomass image.
     :param filename: str
+        Out path.
     """
     with rio.open(driver, 'r') as h1, rio.open(biomass, 'r') as h2:
         driver_data = h1.read(1)
@@ -42,13 +46,21 @@ def worker(driver, biomass, filename):
 
 def agb_emissions(driver, biomass, area=900, co2=3.7, gl30=(10, 25, 30, 40, 50, 60, 70, 80, 90, 100)):
     """
+    Computes the emissions trough above ground biomass removals. Requires deforestation data
+    per pixel and a equal shaped biomass per pixel map.
 
     :param driver: np.array
+        A 2-dimensional numpy array.
     :param biomass: np.array
+        A 2-dimensional numpy array.
     :param area: numeric
+        Area in square meter.
     :param co2: numeric
+        Carbon to carbon dioxide conversion factor.
     :param gl30: tuple of int
+        GL30 classes to consider for emissions computation
     :return: np.array
+        Above ground biomass emissions per pixel.
     """
     ha_per_px = area * 0.0001
 
