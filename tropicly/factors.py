b737d54629f60180b84ca2bb53b0a912c3eaf344
tobijjah <tobias.seydewitz@posteo.net>
2018-07-09 09:15:00 +0200

    Moved socc coefficients to enums + refactoring

--- tropicly/factors.py
+++ tropicly/factors.py
@@ -5,11 +5,30 @@ Author: Tobias Seydewitz
 Date: 16.05.18
 Mail: tobi.seyde@gmail.com
 """
-from tropicly.enums import SOCClasses, GL30Classes
 
 
 class Coefficient:
+    """
+    A data object class stores some statistical properties.
+    Used as coefficients to compute Ecosystem Service Values and
+    Soil Organic Carbon changes.
+    """
     def __init__(self, name, mean, std=None, med=None, mini=None, maxi=None):
+        """
+        Constructor
+
+        :param name: str
+            Identifier
+        :param mean: numeric
+        :param std: numeric, optional
+            Standard deviation
+        :param med: numeric, optional
+            Median
+        :param mini: numeric, optional
+            Minimum, if std true and mini false equals
+            mean - std
+        :param maxi: numeric, optional
+        """
         self.name = name
         self.mean = mean
         self.std = std
@@ -36,71 +55,3 @@ class Coefficient:
     def __repr__(self):
         return '<{}(name={}, mean={}) at {}>'.format(self.__class__.__name__, self.name,
                                                      self.mean, hex(id(self)))
-
-
-class SOCCFactor:
-    def __init__(self, alias, mean, std):
-        self.alias = alias
-        self.mean = mean
-        self.std = std  # standard deviation
-
-    @property
-    def min(self):
-        return self.mean - self.std
-
-    @property
-    def max(self):
-        return self.mean + self.std
-
-    def __repr__(self):
-        return '<{}(alias={}, mean={}, std={}) at {}>'.format(self.__class__.__name__, self.alias,
-                                                              self.mean, self.std, hex(id(self)))
-
-
-# Full depth soil organic carbon change factors
-# Don et al. (2011) Impact of tropical land-use change on soil organic
-# carbon stocks - a meta-analysis, Global Change Biology
-SOCC_data = [
-    SOCCFactor((SOCClasses.primary_forest, SOCClasses.grassland), .121, .023),
-    SOCCFactor((SOCClasses.primary_forest, SOCClasses.cropland), .252, .033),
-    SOCCFactor((SOCClasses.primary_forest, SOCClasses.perennial_crops), .303, .027),
-    SOCCFactor((SOCClasses.primary_forest, SOCClasses.secondary_forest), .086, .02),
-    SOCCFactor((SOCClasses.secondary_forest, SOCClasses.grassland), .064, .025),
-    SOCCFactor((SOCClasses.secondary_forest, SOCClasses.cropland), .213, .041),
-    SOCCFactor((SOCClasses.secondary_forest, SOCClasses.perennial_crops), .024, .042),
-]
-
-SOCCFactors = {
-    (SOCClasses.primary_forest, GL30Classes.cropland): SOCC_data[1],
-    (SOCClasses.primary_forest, GL30Classes.regrowth): SOCC_data[3],
-    (SOCClasses.primary_forest, GL30Classes.grassland): SOCC_data[0],
-    (SOCClasses.primary_forest, GL30Classes.shrubland): SOCC_data[0],
-    (SOCClasses.primary_forest, GL30Classes.tundra): SOCC_data[0],
-    (SOCClasses.primary_forest, GL30Classes.bareland): SOCC_data[0],
-    (SOCClasses.secondary_forest, GL30Classes.cropland): SOCC_data[-2],
-    (SOCClasses.secondary_forest, GL30Classes.grassland): SOCC_data[-3],
-    (SOCClasses.secondary_forest, GL30Classes.shrubland): SOCC_data[-3],
-    (SOCClasses.secondary_forest, GL30Classes.tundra): SOCC_data[-3],
-    (SOCClasses.secondary_forest, GL30Classes.bareland): SOCC_data[-3],
-}
-
-SOCCAlternativeFactors = {
-    (SOCClasses.primary_forest, GL30Classes.cropland): SOCC_data[1],
-    (SOCClasses.primary_forest, GL30Classes.regrowth): SOCC_data[3],
-    (SOCClasses.primary_forest, GL30Classes.grassland): SOCC_data[0],
-    (SOCClasses.primary_forest, GL30Classes.shrubland): SOCC_data[0],
-    (SOCClasses.primary_forest, GL30Classes.tundra): SOCC_data[0],
-    (SOCClasses.primary_forest, GL30Classes.bareland): SOCC_data[0],
-    (SOCClasses.primary_forest, GL30Classes.artificial): SOCC_data[1],
-    (SOCClasses.secondary_forest, GL30Classes.cropland): SOCC_data[-2],
-    (SOCClasses.secondary_forest, GL30Classes.regrowth): SOCC_data[3],
-    (SOCClasses.secondary_forest, GL30Classes.grassland): SOCC_data[-3],
-    (SOCClasses.secondary_forest, GL30Classes.shrubland): SOCC_data[-3],
-    (SOCClasses.secondary_forest, GL30Classes.tundra): SOCC_data[-3],
-    (SOCClasses.secondary_forest, GL30Classes.bareland): SOCC_data[-3],
-    (SOCClasses.secondary_forest, GL30Classes.artificial): SOCC_data[1],
-}
-
-ESV_costanza = {}
-ESV_deGroot = {}
-ESV_worldbank = {}
