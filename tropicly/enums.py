f1d3935c25655ea43da5b136ea99a101a52ef6d0
tobijjah <tobias.seydewitz@posteo.net>
2018-07-09 09:14:15 +0200

    Moved socc coefficients to enums

--- tropicly/enums.py
+++ tropicly/enums.py
@@ -6,6 +6,7 @@ Date: 16.05.18
 Mail: tobi.seyde@gmail.com
 """
 from enum import Enum
+from tropicly.factors import Coefficient
 
 
 class SOCClasses(Enum):
@@ -36,3 +37,52 @@ class GL30Classes(Enum):
     bareland = 90
     snow = 100
     no_data = 255
+
+
+# Full depth soil organic carbon change factors
+# Don et al. (2011) Impact of tropical land-use change on soil organic
+# carbon stocks - a meta-analysis, Global Change Biology
+SOCC_data = [
+    Coefficient((SOCClasses.primary_forest, SOCClasses.grassland), .121, .023),
+    Coefficient((SOCClasses.primary_forest, SOCClasses.cropland), .252, .033),
+    Coefficient((SOCClasses.primary_forest, SOCClasses.perennial_crops), .303, .027),
+    Coefficient((SOCClasses.primary_forest, SOCClasses.secondary_forest), .086, .02),
+    Coefficient((SOCClasses.secondary_forest, SOCClasses.grassland), .064, .025),
+    Coefficient((SOCClasses.secondary_forest, SOCClasses.cropland), .213, .041),
+    Coefficient((SOCClasses.secondary_forest, SOCClasses.perennial_crops), .024, .042),
+]
+
+SOCCFactors = {
+    (SOCClasses.primary_forest, GL30Classes.cropland): SOCC_data[1],
+    (SOCClasses.primary_forest, GL30Classes.regrowth): SOCC_data[3],
+    (SOCClasses.primary_forest, GL30Classes.grassland): SOCC_data[0],
+    (SOCClasses.primary_forest, GL30Classes.shrubland): SOCC_data[0],
+    (SOCClasses.primary_forest, GL30Classes.tundra): SOCC_data[0],
+    (SOCClasses.primary_forest, GL30Classes.bareland): SOCC_data[0],
+    (SOCClasses.secondary_forest, GL30Classes.cropland): SOCC_data[-2],
+    (SOCClasses.secondary_forest, GL30Classes.grassland): SOCC_data[-3],
+    (SOCClasses.secondary_forest, GL30Classes.shrubland): SOCC_data[-3],
+    (SOCClasses.secondary_forest, GL30Classes.tundra): SOCC_data[-3],
+    (SOCClasses.secondary_forest, GL30Classes.bareland): SOCC_data[-3],
+}
+
+SOCCAlternativeFactors = {
+    (SOCClasses.primary_forest, GL30Classes.cropland): SOCC_data[1],
+    (SOCClasses.primary_forest, GL30Classes.regrowth): SOCC_data[3],
+    (SOCClasses.primary_forest, GL30Classes.grassland): SOCC_data[0],
+    (SOCClasses.primary_forest, GL30Classes.shrubland): SOCC_data[0],
+    (SOCClasses.primary_forest, GL30Classes.tundra): SOCC_data[0],
+    (SOCClasses.primary_forest, GL30Classes.bareland): SOCC_data[0],
+    (SOCClasses.primary_forest, GL30Classes.artificial): SOCC_data[1],
+    (SOCClasses.secondary_forest, GL30Classes.cropland): SOCC_data[-2],
+    (SOCClasses.secondary_forest, GL30Classes.regrowth): SOCC_data[3],
+    (SOCClasses.secondary_forest, GL30Classes.grassland): SOCC_data[-3],
+    (SOCClasses.secondary_forest, GL30Classes.shrubland): SOCC_data[-3],
+    (SOCClasses.secondary_forest, GL30Classes.tundra): SOCC_data[-3],
+    (SOCClasses.secondary_forest, GL30Classes.bareland): SOCC_data[-3],
+    (SOCClasses.secondary_forest, GL30Classes.artificial): SOCC_data[1],
+}
+
+ESV_costanza = {}
+ESV_deGroot = {}
+ESV_worldbank = {}
