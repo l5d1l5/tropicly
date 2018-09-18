eb85cbedac7848af801894914723e5eff7ea996d
tobijjah <tobias.seydewitz@posteo.net>
2018-09-18 16:58:16 +0200

    Function visual treecover

--- tropicly/scripts/visualization.py
+++ tropicly/scripts/visualization.py
@@ -6,11 +6,59 @@ Date: 12.07.18
 Mail: tobi.seyde@gmail.com
 """
 import fiona
-from collections import OrderedDict
+import pandas as pd
+import geopandas as gpd
+from bisect import bisect_left
+from math import sqrt, isclose
 from shapely.affinity import scale
 from shapely.geometry import asShape
 from tropicly.grid import SegmentedHexagon
-from math import sqrt, isclose
+
+
+def visual_treecover(path, out, bins=None, unit=1000000, scaling='relative'):
+    if not bins:
+        bins = [i/10 for i in range(1, 11)]
+
+    geometries = []
+    properties = []
+    max_area = 0
+
+    with fiona.open(path, 'r') as src:
+        crs = src.crs
+
+        for feat in src:
+            prop, geo = feat['properties'], feat['geometry']
+
+            prop['ratio'] = prop['covered'] / prop['count']
+            prop['area'] = (prop['covered'] * prop['px_area']) / unit
+
+            if prop['area'] > max_area:
+                max_area = prop['area']
+
+            geometries.append(geo)
+            properties.append(prop)
+
+    scaled_geometries = []
+    for prop, geo in zip(properties, geometries):
+        if scaling == 'relative':
+            factor = bins[bisect_left(bins, prop['ratio'])]
+            poly = scale(asShape(geo), xfact=factor, yfact=factor)
+            prop['scaling'] = factor
+
+        else:
+            ratio = prop['area'] / max_area
+            factor = bins[bisect_left(bins, ratio)]
+            poly = scale(asShape(geo), xfact=factor, yfact=factor)
+            prop['a_ratio'] = ratio
+            prop['scaling'] = factor
+
+        scaled_geometries.append(poly)
+
+    df = pd.DataFrame(properties)
+    gdf = gpd.GeoDataFrame(df, geometry=scaled_geometries)
+    gdf.crs = crs
+    gdf.to_file(out)
+
 
 
 # REFACTOR THIS SHIT TO REUSEABLE FUNCTIONS
@@ -18,8 +66,8 @@ from math import sqrt, isclose
 
 # path = '/home/tobi/Documents/driver_africa_filtered.shp'
 # path = '/home/tobi/Documents/driver_filtered_africa.shp'
-path = '/home/tobi/Documents/driver_normalized_scaled_filtered_africa.shp'
-vec = fiona.open(path)
+# path = '/home/tobi/Documents/driver_normalized_scaled_filtered_africa.shp'
+# vec = fiona.open(path)
 
 # # CALCULATE DRIVERS SCALING
 # schema = vec.schema
@@ -72,83 +120,90 @@ vec = fiona.open(path)
 #         src.write(feature)
 
 
-# CALCULATE DRIVER HEXAGON SEGMENTS
-def error_gen(actual, rounded):
-    divisor = sqrt(1.0 if actual < 1.0 else actual)
-    return abs(rounded - actual) ** 2 / divisor
+# # CALCULATE DRIVER HEXAGON SEGMENTS
+# def error_gen(actual, rounded):
+#     divisor = sqrt(1.0 if actual < 1.0 else actual)
+#     return abs(rounded - actual) ** 2 / divisor
+#
+#
+# def round_to_100(percents):
+#     if not isclose(sum(percents), 100):
+#         raise ValueError
+#
+#     rounded = [int(val) for val in percents]
+#     up_count = 100 - sum(rounded)
+#
+#     errors = [
+#         (error_gen(per, rnd + 1) - error_gen(per, rnd), idx)
+#         for idx, (per, rnd) in enumerate(zip(percents, rounded))
+#     ]
+#
+#     rank = sorted(errors)
+#
+#     for i in range(up_count):
+#         rounded[rank[i][1]] += 1
+#
+#     return rounded
+#
+#
+# schema = {
+#     'geometry': 'Polygon',
+#     'properties': OrderedDict([
+#         ('id', 'int:10'),
+#         ('class', 'str:10'),
+#         ('ratio', 'int:10'),
+#         ('covered', 'int:10'),
+#         ('impact', 'int:10'),
+#         ('im/co', 'float:10.8')
+#     ])
+# }
+#
+# with fiona.open('/home/tobi/Documents/driver_normalized_scaled_ratio_filtered_africa.shp', 'w', schema=schema,
+#                 driver=vec.driver, crs=vec.crs) as dst:
+#     for idx, feature in enumerate(vec):
+#         hexagon = SegmentedHexagon(asShape(feature['geometry']))
+#         properties = feature['properties']
+#         impact = properties['impact']
+#
+#         ratios = []
+#         for driver in ['10', '25', '30', '40', '50', '60', '70', '80', '90', '100']:
+#             if driver in properties:
+#                 if properties[driver] > 0:
+#                     ratio = (properties[driver]/impact)*100
+#                     ratios.append([driver, ratio])
+#
+#         driv, rat = list(zip(*ratios))
+#         rat = round_to_100(rat)
+#         ratios = sorted(zip(driv, rat), key=lambda x: x[1], reverse=True)
+#         for driver, ratio in filter(lambda x: x[1] > 0, ratios):
+#             segment = hexagon.get_segment(ratio)
+#             feat = {
+#                 'geometry': segment.__geo_interface__,
+#                 'properties': OrderedDict([
+#                     ('id', idx),
+#                     ('class', driver),
+#                     ('ratio', ratio),
+#                     ('covered', properties['covered']),
+#                     ('impact', properties['impact']),
+#                     ('im/co', properties['im/co'])
+#                 ])
+#             }
+#             dst.write(feat)
 
 
-def round_to_100(percents):
-    if not isclose(sum(percents), 100):
-        raise ValueError
-
-    rounded = [int(val) for val in percents]
-    up_count = 100 - sum(rounded)
-
-    errors = [
-        (error_gen(per, rnd + 1) - error_gen(per, rnd), idx)
-        for idx, (per, rnd) in enumerate(zip(percents, rounded))
-    ]
-
-    rank = sorted(errors)
-
-    for i in range(up_count):
-        rounded[rank[i][1]] += 1
-
-    return rounded
-
-
-schema = {
-    'geometry': 'Polygon',
-    'properties': OrderedDict([
-        ('id', 'int:10'),
-        ('class', 'str:10'),
-        ('ratio', 'int:10'),
-        ('covered', 'int:10'),
-        ('impact', 'int:10'),
-        ('im/co', 'float:10.8')
-    ])
-}
-
-with fiona.open('/home/tobi/Documents/driver_normalized_scaled_ratio_filtered_africa.shp', 'w', schema=schema,
-                driver=vec.driver, crs=vec.crs) as dst:
-    for idx, feature in enumerate(vec):
-        hexagon = SegmentedHexagon(asShape(feature['geometry']))
-        properties = feature['properties']
-        impact = properties['impact']
-
-        ratios = []
-        for driver in ['10', '25', '30', '40', '50', '60', '70', '80', '90', '100']:
-            if driver in properties:
-                if properties[driver] > 0:
-                    ratio = (properties[driver]/impact)*100
-                    ratios.append([driver, ratio])
-
-        driv, rat = list(zip(*ratios))
-        rat = round_to_100(rat)
-        ratios = sorted(zip(driv, rat), key=lambda x: x[1], reverse=True)
-        for driver, ratio in filter(lambda x: x[1] > 0, ratios):
-            segment = hexagon.get_segment(ratio)
-            feat = {
-                'geometry': segment.__geo_interface__,
-                'properties': OrderedDict([
-                    ('id', idx),
-                    ('class', driver),
-                    ('ratio', ratio),
-                    ('covered', properties['covered']),
-                    ('impact', properties['impact']),
-                    ('im/co', properties['im/co'])
-                ])
-            }
-            dst.write(feat)
-
-
-# CALCULATE COVER SCALING AND SCALE
-# with fiona.open('/home/tobi/Documents/cover_scaled_asia.shp',
-#                 'w', driver=vec.driver, schema=vec.schema, crs=vec.crs) as src:
-#     for feature in vec:
-#         if feature['properties']['covered'] != 0:
-#             ratio = feature['properties']['covered'] / feature['properties']['count']
-#             scaling = int(ratio * 10 + 1) / 10
-#             feature['geometry'] = scale(asShape(feature['geometry']), xfact=scaling, yfact=scaling).__geo_interface__
-#             src.write(feature)
+if __name__ == '__main__':
+    visual_treecover(
+        '/home/tobi/Documents/Master/code/python/Master/data/proc/agg/vector/treecover/americas_cover.shp',
+        '/home/tobi/Documents/Master/code/python/Master/data/proc/agg/vector/treecover/americas_cover_absolute_scaled_10bins.shp',
+        scaling='absolute'
+    )
+    visual_treecover(
+        '/home/tobi/Documents/Master/code/python/Master/data/proc/agg/vector/treecover/africa_cover.shp',
+        '/home/tobi/Documents/Master/code/python/Master/data/proc/agg/vector/treecover/africa_cover_absolute_scaled_10bins.shp',
+        scaling='absolute'
+    )
+    visual_treecover(
+        '/home/tobi/Documents/Master/code/python/Master/data/proc/agg/vector/treecover/asia_cover.shp',
+        '/home/tobi/Documents/Master/code/python/Master/data/proc/agg/vector/treecover/asia_cover_absolute_scaled_10bins.shp',
+        scaling='absolute'
+    )
