b66391c7e35f6ddbdc3b56c77f1681fece9ca125
tobijjah <tobias.seydewitz@posteo.net>
2018-09-21 14:53:25 +0200

    Initial commit

---
+++ tropicly/scripts/esv.py
@@ -0,0 +1,48 @@
+"""
+esv.py
+
+Author: Tobias Seydewitz
+Date: 21.09.18
+Mail: tobi.seyde@gmail.com
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+src = pd.read_csv('/home/tobi/Documents/Master/code/python/Master/data/proc/ana/esv.csv').values
+
+fig, axes = plt.subplots(ncols=3, sharey=True, figsize=(5.5, 3.6))
+fig.subplots_adjust(wspace=0.1)
+
+for ax, row in zip(axes, src):
+    ax.bar(
+        [i for i in range(1, 4)],
+        row[1::2]/(10**9),
+        width=-0.4,
+        align='edge',
+        color='#fc8d59',
+        label='Loss'
+    )
+    ax.bar(
+        [i for i in range(1, 4)],
+        row[2::2]/(10**9),
+        width=0.4, align='edge', color='#99d594',
+        label='Gain'
+    )
+
+    ax.set_xticklabels(
+        ['', 'Costanza', 'DeGroot', 'Worldbank'],
+        fontsize=10,
+        fontname='Times new roman',
+        rotation=60
+    )
+    ax.set_xlabel(row[0].capitalize(), fontsize=12, fontname='Times new roman')
+
+axes[0].set_ylabel('Ecosystem service value [billion Int$/y]', fontsize=12, fontname='Times new roman')
+axes[-1].legend()
+
+plt.show()
+fig.savefig(
+    '/home/tobi/Documents/Master/code/python/Master/doc/thesis/img/esv.png',
+    format='png'
+)
---
+++ tropicly/scripts/jaccard.py
@@ -0,0 +1,54 @@
+"""
+jaccard.py
+
+Author: Tobias Seydewitz
+Date: 20.09.18
+Mail: tobi.seyde@gmail.com
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+src = pd.read_csv('/home/tobi/Documents/Master/code/python/Master/data/proc/ana/harmonization_americas.csv')
+src = src.append(pd.read_csv('/home/tobi/Documents/Master/code/python/Master/data/proc/ana/harmonization_africa.csv'))
+src = src.append(pd.read_csv('/home/tobi/Documents/Master/code/python/Master/data/proc/ana/harmonization_asia.csv'))
+
+# initial data clean up
+zeros = src[src['JC00'] == 0]
+src.drop(zeros.index, axis=0, inplace=True)
+src.rename(columns=lambda x: x.upper() if x[:2] == 'jc' else x, inplace=True)
+src.dropna(axis=0, how='any', inplace=True)
+src.columns = 'JC0 JC10 JC20 JC30 tile region'.split()
+
+fig, axes = plt.subplots(ncols=3, sharey=True, figsize=(5.5, 3))
+fig.subplots_adjust(wspace=0.1)
+
+for ax, group in zip(axes, src.groupby(by='region')):
+    region, df = group
+
+    boxes = ax.boxplot(
+        [row for row in df.values.T[0:4]],
+        showmeans=True,
+        sym='.',
+        patch_artist=True
+    )
+    ax.set_xticklabels(['JC0', 'JC10', 'JC20', 'JC30'], fontsize=10, fontname='Times new roman')
+    #ax.grid(True, axis='y')
+    ax.set_xlabel(region, fontsize=12, fontname='Times new roman')
+
+    for box, med, mean in zip(boxes['boxes'], boxes['medians'], boxes['means']):
+        box.set_facecolor('white')
+        med.set_color('black')
+        mean.set_marker('x')
+        mean.set_markeredgecolor('red')
+
+
+axes[0].set_yticks([tick / 10 for tick in range(11)])
+axes[0].set_ylabel('Jaccard score', fontsize=12, fontname='Times new roman')
+axes[0].set_ylim(bottom=-.01, top=1.)
+
+plt.show()
+fig.savefig(
+    '/home/tobi/Documents/Master/code/python/Master/doc/thesis/img/jaccard.png',
+    format='png'
+)
