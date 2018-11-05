fc8a628b0b3e8e6d0304ed22392c807d51ebb0ac
tobijjah <tobias.seydewitz@posteo.net>
2018-11-05 14:56:29 +0100

    Writing

---
+++ tropicly/r/treecover_similarity.R
@@ -0,0 +1,22 @@
+jc_americas = read.csv('/home/tobi/Desktop/proj/data/proc/ana/harmonization_americas.csv')
+americas = jc_americas[jc_americas[,1]!=0,]
+americas = americas[,1:4]
+cor(americas[,1:4])
+summary(americas[,1:4])
+
+jc_africa = read.csv('/home/tobi/Desktop/proj/data/proc/ana/harmonization_africa.csv')
+africa = jc_africa[jc_africa[,1]!=0,]
+africa = africa[,1:4]
+cor(africa[,1:4])
+summary(africa[,1:4])
+
+jc_asia = read.csv('/home/tobi/Desktop/proj/data/proc/ana/harmonization_asia.csv')
+asia = jc_asia[jc_asia[,1]!=0,]
+asia = asia[,1:4]
+cor(asia[,1:4])
+summary(asia[,1:4])
+
+
+wilcox.test(asia$JC00, asia$JC30, paired = TRUE, alternative = 'two.sided')
+
+
