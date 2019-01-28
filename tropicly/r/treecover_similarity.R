c2479f025021252b2619c61796abb8ba40cdf9b3
tobijjah <tobias.seydewitz@posteo.net>
2019-01-28 15:22:38 +0100

    NA

--- tropicly/r/treecover_similarity.R
+++ tropicly/r/treecover_similarity.R
@@ -30,9 +30,8 @@ less = data.frame(JC00=double(),
                   JC30=double())
 for(i in 1:4) {
   for(j in 1:4) {
-    equal[j, i] = wilcox.test(africa[,i], africa[,j], paired = TRUE, alternative = 'two.sided')$p.value
-    greater[j, i] = wilcox.test(africa[,i], africa[,j], paired = TRUE, alternative = 'greater')$p.value
-    less[j, i] = wilcox.test(africa[,i], africa[,j], paired = TRUE, alternative = 'less')$p.value
+    equal[j, i] = wilcox.test(asia[,i], asia[,j], paired = TRUE, alternative = 'two.sided')$p.value
+    greater[j, i] = wilcox.test(asia[,i], asia[,j], paired = TRUE, alternative = 'greater')$p.value
+    less[j, i] = wilcox.test(asia[,i], asia[,j], paired = TRUE, alternative = 'less')$p.value
   }
 }
-
