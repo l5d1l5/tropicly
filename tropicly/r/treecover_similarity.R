f3e556cff57fd1eb51f75575c3937733fe5bea0d
tobijjah <tobias.seydewitz@posteo.net>
2019-02-01 12:52:29 +0100

    Writing

--- tropicly/r/treecover_similarity.R
+++ tropicly/r/treecover_similarity.R
@@ -1,20 +1,23 @@
-jc_americas = read.csv('/home/tobi/Desktop/proj/data/proc/ana/harmonization_americas.csv')
+jc_americas = read.csv('/home/tobi/Documents/tropicly/data/proc/ana/harmonization_americas.csv')
 americas = jc_americas[jc_americas[,1]!=0,]
 americas = americas[,1:4]
 #cor(americas[,1:4])
 summary(americas[,1:4])
 
-jc_africa = read.csv('/home/tobi/Desktop/proj/data/proc/ana/harmonization_africa.csv')
+jc_africa = read.csv('/home/tobi/Documents/tropicly/data/proc/ana/harmonization_africa.csv')
 africa = jc_africa[jc_africa[,1]!=0,]
 africa = africa[,1:4]
 #cor(africa[,1:4])
 summary(africa[,1:4])
 
-jc_asia = read.csv('/home/tobi/Desktop/proj/data/proc/ana/harmonization_asia.csv')
+jc_asia = read.csv('/home/tobi/Documents/tropicly/data/proc/ana/harmonization_asia.csv')
 asia = jc_asia[jc_asia[,1]!=0,]
 asia = asia[,1:4]
 #cor(asia[,1:4])
 summary(asia[,1:4])
+
+all_regions = rbind(americas, asia)
+all_regions = rbind(all_regions, africa)
 
 equal = data.frame(JC00=double(),
                    JC10=double(),
@@ -30,8 +33,9 @@ less = data.frame(JC00=double(),
                   JC30=double())
 for(i in 1:4) {
   for(j in 1:4) {
-    equal[j, i] = wilcox.test(asia[,i], asia[,j], paired = TRUE, alternative = 'two.sided')$p.value
-    greater[j, i] = wilcox.test(asia[,i], asia[,j], paired = TRUE, alternative = 'greater')$p.value
-    less[j, i] = wilcox.test(asia[,i], asia[,j], paired = TRUE, alternative = 'less')$p.value
+    equal[j, i] = wilcox.test(africa[,i], africa[,j], paired = TRUE, alternative = 'two.sided')$p.value
+    greater[j, i] = wilcox.test(africa[,i], africa[,j], paired = TRUE, alternative = 'greater')$p.value
+    less[j, i] = wilcox.test(africa[,i], africa[,j], paired = TRUE, alternative = 'less')$p.value
   }
 }
+
