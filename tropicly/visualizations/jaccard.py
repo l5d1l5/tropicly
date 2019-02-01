c320593cfe8ee2331cad881da9406f7e901a3ecf
tobijjah <tobias.seydewitz@posteo.net>
2019-02-01 13:00:47 +0100

    Include all regions

--- tropicly/visualizations/jaccard.py
+++ tropicly/visualizations/jaccard.py
@@ -15,14 +15,13 @@ src = src.append(pd.read_csv(DIRS.ana / 'harmonization_asia.csv'), ignore_index=
 zeros = src[src['JC00'] == 0]
 src.drop(zeros.index, axis=0, inplace=True)
 
-fig, axes = plt.subplots(ncols=3, sharey=True, figsize=(5.5, 3))
+fig, axes = plt.subplots(ncols=4, sharey=True, figsize=(5.5, 3))
 fig.subplots_adjust(wspace=0.1)
 
 groups = src.groupby(by='region', sort=True)
 
 for ax, region in zip(axes, ['Americas', 'Asia', 'Africa']):
     df = groups.get_group(region)
-    print(len(df))
 
     boxes = ax.boxplot(
         [row for row in df.values.T[0:4]],
@@ -39,6 +38,22 @@ for ax, region in zip(axes, ['Americas', 'Asia', 'Africa']):
         mean.set_marker('x')
         mean.set_markeredgecolor('red')
 
+###### TODO refactor
+boxes = axes[-1].boxplot(
+    [row for row in src.values.T[0:4]],
+    showmeans=True,
+    sym='.',
+    patch_artist=True
+)
+
+axes[-1].set_xticklabels(['JI$_0$', 'JI$_1$', 'JI$_2$', 'JI$_3$'], fontsize=10, fontname='Times new roman')
+axes[-1].set_title('All regions', fontsize=12, fontname='Times new roman')
+for box, med, mean in zip(boxes['boxes'], boxes['medians'], boxes['means']):
+    box.set_facecolor('white')
+    med.set_color('black')
+    mean.set_marker('x')
+    mean.set_markeredgecolor('red')
+#######
 
 axes[0].set_yticks([tick / 10 for tick in range(11)])
 axes[0].set_ylabel('Jaccard Index', fontsize=12, fontname='Times new roman')
