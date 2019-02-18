d35be76509e62c08aff5e7a64cec266362b21082
tobijjah <tobias.seydewitz@posteo.net>
2019-02-18 13:16:20 +0100

    Writing

--- tropicly/r/treecover_similarity.R
+++ tropicly/r/treecover_similarity.R
@@ -1,16 +1,16 @@
-jc_americas = read.csv('/home/ilex/Documents/code/python/proj/tropicly/data/proc/ana/harmonization_sort_americas.csv')
+jc_americas = read.csv('/home/tobi/Documents/tropicly/data/proc/agg/textual/harmonization_sort_americas.csv')
 americas = jc_americas[jc_americas[,1]!=0,]
 americas = americas[,1:4]
 #cor(americas[,1:4])
 summary(americas[,1:4])
 
-jc_africa = read.csv('/home/ilex/Documents/code/python/proj/tropicly/data/proc/ana/harmonization_sort_africa.csv')
+jc_africa = read.csv('/home/tobi/Documents/tropicly/data/proc/agg/textual/harmonization_sort_africa.csv')
 africa = jc_africa[jc_africa[,1]!=0,]
 africa = africa[,1:4]
 #cor(africa[,1:4])
 summary(africa[,1:4])
 
-jc_asia = read.csv('/home/ilex/Documents/code/python/proj/tropicly/data/proc/ana/harmonization_sort_asia.csv')
+jc_asia = read.csv('/home/tobi/Documents/tropicly/data/proc/agg/textual/harmonization_sort_asia.csv')
 asia = jc_asia[jc_asia[,1]!=0,]
 asia = asia[,1:4]
 #cor(asia[,1:4])
@@ -39,16 +39,18 @@ for(i in 1:4) {
   }
 }
 
-x_range = 1:length(americas$JC00)
-
-plot(x_range, americas$JC00, pch=1, ylim=0:1, xlab='AISM tile', ylab='Jaccard Index')
+png('/home/tobi/Documents/tropicly/data/proc/fig/jaccard_tiles_africa.png', width=11.7, height=8.3, units='in', res=300)
+x_range = 1:length(africa$JC00)
+plot(x_range, africa$JC00, pch=1, ylim=0:1, xlab='AISM tile', ylab='Jaccard Index')
 par(new=TRUE)
-plot(x_range, americas$JC10, axes=FALSE, pch=0, xlab='', ylab='')
+plot(x_range, africa$JC10, axes=FALSE, pch=0, xlab='', ylab='')
 par(new=TRUE)
-plot(x_range, americas$JC20, axes=FALSE, pch=2, xlab='', ylab='')
+plot(x_range, africa$JC20, axes=FALSE, pch=2, xlab='', ylab='')
 par(new=TRUE)
-plot(x_range, americas$JC30, axes=FALSE, pch=3, xlab='', ylab='')
+plot(x_range, africa$JC30, axes=FALSE, pch=3, xlab='', ylab='')
 par(new=FALSE)
+dev.off()
+
 
 test = c(americas$JC00, americas$JC10, americas$JC20, americas$JC30)
 labels = rep(c('JI0', 'JI1', 'JI2', 'JI3'), each=length(americas$JC00))
