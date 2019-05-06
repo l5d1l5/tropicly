d7cd4db732a29009ef2d7b4fc2349ba208df3758
tobijjah <tobias.seydewitz@posteo.net>
2019-05-06 13:55:23 +0200

    Add cover_classes, canopy_densities

--- tropicly/settings.py
+++ tropicly/settings.py
@@ -13,4 +13,6 @@ SETTINGS = {
     'headers': {'headers': {'User-Agent': "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}},
     'wgs84': CRS.from_epsg(4326),
     'data': cache_directories(get_data_dir()),
+    'canopy_densities': list(range(0, 100, 5)),
+    'cover_classes': [20],
 }
