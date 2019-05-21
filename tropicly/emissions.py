b58f5e64cd4ba8ac04e10427b78c6511fc60fcbf
tobijjah <tobias.seydewitz@posteo.net>
2019-05-21 14:15:47 +0200

    Initial commit

---
+++ tropicly/emissions.py
@@ -0,0 +1,9 @@
+"""
+Module: emissions.py
+****
+
+:Author: Tobias Seydewitz
+:Date: 21.05.19
+:Mail: seydewitz@pik-potsdam.de
+:Institution: `Potsdam Institute for Climate Impact Research (PIK) <https://www.pik-potsdam.de/>`_
+"""
