814887458c0a6fea8e391e51e7a55cd5ec2ce66b
tobijjah <tobias.seydewitz@posteo.net>
2019-06-04 15:21:19 +0200

    Update test

--- tests/test_emissions.py
+++ tests/test_emissions.py
@@ -1,20 +1,24 @@
-import numpy as np
 from unittest import TestCase
-from legacy.enums import SOCClasses
-from legacy.soc_emissions import factor_map, soc_emissions
+
+import numpy as np
+
+from emissions import factor_map
+from emissions import soc_emissions
+from settings import SOCClasses
 
 
-class TestSOCEmissions(TestCase):
+class TestEmissions(TestCase):
     def setUp(self):
         np.random.seed(42)
-        self.driver = np.array([0, 10, 20, 25, 30, 40, 50,
-                                60, 70, 80, 90, 100, 255], dtype=np.uint8)
+        self.driver = np.array([0, 10, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 255], dtype=np.uint8)
         self.intact = np.array([1]*13+[0]*13, dtype=np.uint8)
         self.soc = np.round_(np.random.rand(13) * 100, decimals=2).astype(np.float32)
-        self.f1 = np.array([0, .213, 0, 0, .064, .064, 0,
-                            0, .064, 0, .064, 0, 0], dtype=np.float32)
-        self.f2 = np.array([0, .252, 0, .086, .121, .121, 0,
-                            0, .121, 0, .121, 0, 0], dtype=np.float32)
+        self.f1 = np.array([[0, .172, 0, 0, .039, .039, 0, 0, .039, 0, .039, 0, 0],
+                            [0, .213, 0, 0, .064, .064, 0, 0, .064, 0, .064, 0, 0],
+                            [0, .254, 0, 0, .089, .089, 0, 0, .089, 0, .089, 0, 0]], dtype=np.float32)  # secondary
+        self.f2 = np.array([[0, .219, 0, .066, .098, .098, 0, 0, .098, 0, .098, 0, 0],
+                            [0, .252, 0, .086, .121, .121, 0, 0, .121, 0, .121, 0, 0],
+                            [0, .285, 0, .106, .144, .144, 0, 0, .144, 0, .144, 0, 0]], dtype=np.float32)  # primary
 
     def test_factor_map_secondary(self):
         expected = self.f1
@@ -29,28 +33,28 @@ class TestSOCEmissions(TestCase):
         self.assertTrue(np.array_equal(expected, actual))
 
     def test_factor_map_intact(self):
-        expected = np.concatenate((self.f2, self.f1))
+        expected = np.concatenate((self.f2, self.f1), axis=1)
         actual = factor_map(np.concatenate((self.driver, self.driver)), self.intact)
 
         self.assertTrue(np.array_equal(expected, actual))
 
     def test_soc_emissions_secondary(self):
-        expected = np.round(0.09 * 3.7 * self.soc * self.f1, decimals=2).astype(np.float32)
+        expected = np.round(0.09 * self.soc * self.f1, decimals=2).astype(np.float32)
         actual = soc_emissions(self.driver, self.soc)
 
         self.assertTrue(np.array_equal(expected, actual))
 
     def test_soc_emissions_primary(self):
-        expected = np.round(0.09 * 3.7 * self.soc * self.f2, decimals=2).astype(np.float32)
+        expected = np.round(0.09 * self.soc * self.f2, decimals=2).astype(np.float32)
         actual = soc_emissions(self.driver, self.soc, intact=np.ones(13))
 
         self.assertTrue(np.array_equal(expected, actual))
 
     def test_soc_emissions_intact(self):
-        primary = np.round(0.09 * 3.7 * self.soc * self.f2, decimals=2).astype(np.float32)
-        secondary = np.round(0.09 * 3.7 * self.soc * self.f1, decimals=2).astype(np.float32)
+        primary = np.round(0.09 * self.soc * self.f2, decimals=2).astype(np.float32)
+        secondary = np.round(0.09 * self.soc * self.f1, decimals=2).astype(np.float32)
 
-        expected = np.concatenate((primary, secondary))
+        expected = np.concatenate((primary, secondary), axis=1)
         actual = soc_emissions(np.concatenate((self.driver, self.driver)), np.concatenate((self.soc, self.soc)),
                                intact=self.intact)
 
